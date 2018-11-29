a8d1c50451daa5d0af9baac37229c2ae8430380b
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-11-29 17:38:06 +0900

    update inistall.sh

--- install.sh
+++ install.sh
@@ -24,3 +24,37 @@ docker run --detach --hostname ldap.pettra.com --publish 389:389 --publish 689:6
 
 # openLDAP 검증
 ldapsearch -x -H ldap:// -b dc=pettra,dc=com -D "cn=admin,dc=pettra,dc=com" -w Pettra@1023
+
+# 아래와 같은 결과가 출력되어야 한다.
+# 여기부터
+# extended LDIF
+#
+# LDAPv3
+# base <dc=pettra,dc=com> with scope subtree
+# filter: (objectclass=*)
+# requesting: ALL
+#
+
+# pettra.com
+#dn: dc=pettra,dc=com
+#objectClass: top
+#objectClass: dcObject
+#objectClass: organization
+#o: Example Inc.
+#dc: pettra
+
+# admin, pettra.com
+#dn: cn=admin,dc=pettra,dc=com
+#objectClass: simpleSecurityObject
+#objectClass: organizationalRole
+#cn: admin
+#description: LDAP administrator
+#userPassword:: e1NTSEF9Ui96U0ppdzJKdHhud0N6RGZxb0pacnREMHBQR3Q5M3k=
+
+# search result
+#search: 2
+#result: 0 Success
+
+# numResponses: 3
+# numEntries: 2
+# 여기까지
