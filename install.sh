329c28229d2feb35828cc8cbb077bd7784283a9a
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-11-29 18:17:33 +0900

    update install.sh
    add user info and search command for testing

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ docker run --detach --hostname ldap.pettra.com --publish 389:389 --publish 689:6
 ldapsearch -x -H ldap://ldap.pettra.com -b dc=pettra,dc=com -D "cn=admin,dc=pettra,dc=com" -w Pettra@1023
 
 # 아래와 같은 결과가 출력되어야 한다.
-# 여기부터
+# 여기부터 <============================================================================
 # extended LDIF
 #
 # LDAPv3
@@ -57,7 +57,7 @@ ldapsearch -x -H ldap://ldap.pettra.com -b dc=pettra,dc=com -D "cn=admin,dc=pett
 
 # numResponses: 3
 # numEntries: 2
-# 여기까지
+# 여기까지 ============================================================================>
 
 # phpldapadmin 설치
 # docker run --detach \
@@ -76,3 +76,103 @@ docker run --detach --hostname phpldapadmin.pettra.com --publish 6443:443 --name
 docker cp ./custom_posixAccount.xml phpldapadmin:./var/www/phpldapadmin/templates/creation/
 
 # 브라우져로 phpldapadmin.pettra.com:6443 (or IPAdress:6443)으로 접속
+
+# 사용자 추가 후 추가한 단순 검색 결과
+#ldapsearch -x -H ldap://ldap.pettra.com -b dc=pettra,dc=com -D "cn=admin,dc=pettra,dc=com" -w Pettra@1023
+# 아래와 같은 결과가 출력되어야 한다.
+# 여기부터 <============================================================================
+# extended LDIF
+#
+# LDAPv3
+# base <dc=pettra,dc=com> with scope subtree
+# filter: (objectclass=*)
+# requesting: ALL
+#
+
+# pettra.com
+#dn: dc=pettra,dc=com
+#objectClass: top
+#objectClass: dcObject
+#objectClass: organization
+#o: Example Inc.
+#dc: pettra
+
+# admin, pettra.com
+#dn: cn=admin,dc=pettra,dc=com
+#objectClass: simpleSecurityObject
+#objectClass: organizationalRole
+#cn: admin
+#description: LDAP administrator
+#userPassword:: e1NTSEF9Ui96U0ppdzJKdHhud0N6RGZxb0pacnREMHBQR3Q5M3k=
+
+# groups, pettra.com
+#dn: ou=groups,dc=pettra,dc=com
+#ou: groups
+#objectClass: organizationalUnit
+#objectClass: top
+
+# users, pettra.com
+#dn: ou=users,dc=pettra,dc=com
+#ou: users
+#objectClass: organizationalUnit
+#objectClass: top
+
+# admin, groups, pettra.com
+#dn: cn=admin,ou=groups,dc=pettra,dc=com
+#cn: admin
+#gidNumber: 500
+#objectClass: posixGroup
+#objectClass: top
+
+# dev, groups, pettra.com
+#dn: cn=dev,ou=groups,dc=pettra,dc=com
+#cn: dev
+#gidNumber: 501
+#objectClass: posixGroup
+#objectClass: top
+
+# Roy Kang, users, pettra.com
+#dn: cn=Roy Kang,ou=users,dc=pettra,dc=com
+#givenName: Roy
+#sn: Kang
+#cn: Roy Kang
+#uid: roykang
+#userPassword:: e01ENX13dW10NlRySFRaaEFrUG56YlBEOWNnPT0=
+#uidNumber: 1000
+#gidNumber: 501
+#homeDirectory: /home/users/roykang
+#loginShell: /bin/bash
+#mobile: 010-5495-2654
+#mail: roykang@dogtra.com
+#displayName:: 6rCV7ISx7YOc
+#objectClass: inetOrgPerson
+#objectClass: posixAccount
+#objectClass: top
+
+# search result
+#search: 2
+#result: 0 Success
+
+# numResponses: 8
+# numEntries: 7
+# 여기까지 ============================================================================>
+
+# 사용자 추가 후 추가한 사용자 검증 방법
+ldapsearch -x -H ldap://ldap.pettra.com -b dc=pettra,dc=com -D "cn=Roy Kang,ou=users,dc=pettra,dc=com" -w kstkmr2010
+
+# 아래와 같은 결과가 출력되어야 한다.
+# 여기부터 <============================================================================
+# extended LDIF
+#
+# LDAPv3
+# base <dc=pettra,dc=com> with scope subtree
+# filter: (objectclass=*)
+# requesting: ALL
+#
+
+# search result
+#search: 2
+#result: 32 No such object
+
+# numResponses: 1
+# 여기까지 ============================================================================>
