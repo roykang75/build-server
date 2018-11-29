62730e9aa7b12db7265f4a0094bde1becb2163a4
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-11-29 17:13:50 +0900

    Update install.sh
    add openLDAP install script for docker

--- install.sh
+++ install.sh
@@ -5,3 +5,17 @@ sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 cat /etc/localtime
 
 # openLDAP 설치
+# docker run --detach \
+#    --hostname ldap.pettra.com \
+#    --publish 389:389 --publish 689:689 \
+#    --name openLDAP \
+#    --restart always \
+#    --volume /data/srv/slapd/config:/etc/ldap/slapd.d \
+#    --volume /data/srv/slapd/config:/etc/ldap/slapd.d \
+#    --volume /etc/localtime:/etc/localtime:ro \
+#    --env LDAP_DOMAIN="pettra.com" \
+#    --env LDAP_ADMIN_PASSWORD="Pettra@1023"
+#    osixia/openldap:1.2.2
+
+docker run --detach --hostname ldap.pettra.com --publish 389:389 --publish 689:689 --name openLDAP --restart always --volume /data/srv/slapd/config:/etc/ldap/slapd.d --volume /data/srv/slapd/config:/etc/ldap/slapd.d --volume /etc/localtime:/etc/localtime:ro --env  LDAP_DOMAIN="pettra.com" --env LDAP_ADMIN_PASSWORD="Pettra@1023" osixia/openldap:1.2.2
+
